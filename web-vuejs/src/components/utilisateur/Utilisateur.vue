1d44dbe211fdb1d50a939aab98f02b857c2281b3
Guillaume TALBOT <guillaume.talbot@cgi.com>
2018-01-17 19:14:01 +0100

    Creation d'un composant utilisateur

---
+++ web-vuejs/src/components/utilisateur/Utilisateur.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+
+    <div class="erreur">{{ messageErreur }}</div>
+
+    <h2>Administration des utilisateurs</h2>
+
+    <div v-for="utilisateur in utilisateurs" :key="utilisateur.login">
+      <span>{{ utilisateur.login }}</span>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Utilisateur',
+  data () {
+    return {
+      messageErreur: '',
+      utilisateurs: []
+    }
+  },
+  mounted: function () {
+    this.rechercher()
+  },
+  methods: {
+    rechercher (event) {
+      this.$http.get('http://localhost:9090/applicationBlanche/utilisateur').then(response => {
+        this.utilisateurs = response.body
+      }, response => {
+        this.messageErreur = response.body
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- web-vuejs/src/router/index.js
+++ web-vuejs/src/router/index.js
@@ -1,13 +1,21 @@
+// Import de VueJS et ses modules
 import Vue from 'vue'
 import Router from 'vue-router'
-import Connexion from '@/components/connexion/Connexion'
 import VueResource from 'vue-resource'
 
+// Import des composants de l'application
+import Connexion from '@/components/connexion/Connexion'
+import Utilisateur from '@/components/utilisateur/Utilisateur'
+
+// Déclaration des modules de VueJS
 Vue.use(Router)
 Vue.use(VueResource)
 
+// Définition des routes
 export default new Router({
   routes: [
-    { path: '/', name: 'Connexion', component: Connexion }
+    { path: '/', name: 'Connexion', component: Connexion },
+    { path: '/login', name: 'Connexion', component: Connexion },
+    { path: '/utilisateur', name: 'Utilisateur', component: Utilisateur }
   ]
 })
