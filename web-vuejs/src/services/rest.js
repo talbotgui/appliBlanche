31c88d92c55655d8efdd428c829784393a245421
TALBOT Guillaume <talbotgui@gmail.com>
2018-02-08 07:50:37 +0100

    Définition d'une callback d'erreur par défaut

--- web-vuejs/src/services/rest.js
+++ web-vuejs/src/services/rest.js
@@ -7,17 +7,36 @@ export default {
     utilisateur: null
   },
 
-  getRoles (sucessCallback, erreurCallback) {
+  erreurCallbackParDefaut: reponse => {
+    var message = ''
+    if (reponse) {
+      if (reponse.body && reponse.body.message !== '') {
+        message = reponse.body.message
+      } else if (reponse.bodyText) {
+        message = reponse.bodyText
+      } else if (reponse.ok === false) {
+        message = 'L\'API ne répond pas'
+      } else {
+        console.error(reponse)
+        message = 'Une erreur inconnue est survenue'
+      }
+    } else {
+      message = 'Une erreur inconnue est survenue'
+    }
+    window.alert(message)
+  },
+
+  getRoles (sucessCallback, erreurCallback = this.erreurCallbackParDefaut) {
     const options = { method: 'GET', url: 'v1/roles' }
     Vue.http(options).then(sucessCallback, erreurCallback)
   },
 
-  getUtilisateurs (sucessCallback, erreurCallback) {
+  getUtilisateurs (sucessCallback, erreurCallback = this.erreurCallbackParDefaut) {
     const options = { method: 'GET', url: 'v1/utilisateurs' }
     Vue.http(options).then(sucessCallback, erreurCallback)
   },
 
-  getUtilisateurMoi (sucessCallback, erreurCallback) {
+  getUtilisateurMoi (sucessCallback, erreurCallback = this.erreurCallbackParDefaut) {
     const options = { method: 'GET', url: 'v1/utilisateurs/moi' }
     Vue.http(options).then(
       reponseSucces => { this.estConnecte = true; this.utilisateur = reponseSucces.data; sucessCallback(reponseSucces) },
@@ -25,12 +44,12 @@ export default {
     )
   },
 
-  postRole (param, sucessCallback, erreurCallback) {
+  postRole (param, sucessCallback, erreurCallback = this.erreurCallbackParDefaut) {
     const options = { method: 'POST', url: 'v1/roles', body: param }
     Vue.http(options).then(sucessCallback, erreurCallback)
   },
 
-  postUtilisateur (param, sucessCallback, erreurCallback) {
+  postUtilisateur (param, sucessCallback, erreurCallback = this.erreurCallbackParDefaut) {
     const options = { method: 'POST', url: 'v1/utilisateurs', body: param }
     Vue.http(options).then(sucessCallback, erreurCallback)
   },
