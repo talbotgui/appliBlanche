f9dfbe8328d29a2d2b968c9b4d8958cb8a8be484
TALBOT Guillaume <talbotgui@gmail.com>
2018-04-26 08:22:50 +0200

    Appels REST avec JWT oppérationnels

--- web-vuejs/src/services/rest.js
+++ web-vuejs/src/services/rest.js
@@ -3,7 +3,6 @@ import Vue from 'vue'
 export default {
 
   data: {
-    estConnecte: false,
     utilisateur: null
   },
 
@@ -39,7 +38,7 @@ export default {
   getUtilisateurMoi (sucessCallback, erreurCallback = this.erreurCallbackParDefaut) {
     const options = { method: 'GET', url: 'v1/utilisateurs/moi' }
     Vue.http(options).then(
-      reponseSucces => { this.estConnecte = true; this.utilisateur = reponseSucces.data; sucessCallback(reponseSucces) },
+      reponseSucces => { this.utilisateur = reponseSucces.data; sucessCallback(reponseSucces) },
       erreurCallback
     )
   },
@@ -52,9 +51,13 @@ export default {
     const options = { method: 'POST', url: 'login', body: { login: login, mdp: mdp } }
     Vue.http(options).then(
       reponseSucces => {
-        this.estConnecte = true
-        console.debug(reponseSucces)
-        sucessCallback(reponseSucces)
+        // Lecture du token, sauvegarde et utilisation dans les entêtes de requête
+        const token = reponseSucces.headers.map.authorization[0]
+        localStorage.setItem('JWT', token)
+        Vue.http.headers.common['Authorization'] = token
+
+        // Chargement des informations personnelles
+        this.getUtilisateurMoi(sucessCallback)
       },
       erreurCallback
     )
@@ -71,7 +74,7 @@ export default {
   },
 
   isUtilisateurConnecte () {
-    return this.estConnecte
+    return !!localStorage.getItem('JWT')
   }
 
 }
