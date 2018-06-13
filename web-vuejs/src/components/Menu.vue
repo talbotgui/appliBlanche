2873d01d454d2c6815a4f380bfb2f6580152fb8d
TALBOT Guillaume <talbotgui@gmail.com>
2018-06-13 18:34:13 +0200

    Ajout de couleur dans le menu

--- web-vuejs/src/components/Menu.vue
+++ web-vuejs/src/components/Menu.vue
@@ -2,10 +2,13 @@
   <div>
     <div class="bienvenue">Bonjour</div>
 
-    <h3>Accueil</h3>
-    <router-link to="/administration/utilisateur">Utilisateurs</router-link>
-    <router-link to="/administration/role">Roles</router-link>
-    <a href="#" v-on:click.prevent="deconnecter">Déconnexion</a>
+    <div id="menu" class="row">
+      <nav mat-tab-nav-bar>
+        <router-link to="/administration/utilisateur">Utilisateurs</router-link>
+        <router-link to="/administration/role">Roles</router-link>
+        <a href="#" v-on:click.prevent="deconnecter">Déconnexion</a>
+      </nav>
+    </div>
   </div>
 </template>
 
@@ -31,5 +34,37 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+#menu {
+  background-color: #fff;
+  border: 0;
+  margin-bottom: 50px;
+}
+#menu {
+  border-top: 5px solid #d10000;
+}
+#menu a:link,
+a:visited {
+  border-radius: 0 0 8px 8px;
+  padding: 0px 15px;
+}
+#menu ul {
+  margin: 0;
+  margin-left: 20px;
+  padding: 0;
+}
+#menu li {
+  text-transform: uppercase;
+}
+#menu a,
+#menu router-link {
+  float: left;
+  font-weight: bold;
+  color: #000;
+  margin: 0 5px 0 0;
+  background-color: #eee;
+}
+#menu a:hover {
+  background-color: #d10000;
+  color: #fff;
+}
 </style>
