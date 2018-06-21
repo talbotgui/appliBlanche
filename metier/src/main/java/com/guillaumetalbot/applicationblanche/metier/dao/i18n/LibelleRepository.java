6aac92bfa2403fecfb4d23abd4aa26e80121fad4
TALBOT Guillaume <talbotgui@gmail.com>
2018-06-21 17:44:25 +0200

    Ajout d'un READ_ONLY manquant

--- metier/src/main/java/com/guillaumetalbot/applicationblanche/metier/dao/i18n/LibelleRepository.java
+++ metier/src/main/java/com/guillaumetalbot/applicationblanche/metier/dao/i18n/LibelleRepository.java
@@ -2,7 +2,10 @@ package com.guillaumetalbot.applicationblanche.metier.dao.i18n;
 
 import java.util.Collection;
 
+import javax.persistence.QueryHint;
+
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -11,6 +14,7 @@ import com.guillaumetalbot.applicationblanche.metier.entite.i18n.Libelle;
 public interface LibelleRepository extends CrudRepository<Libelle, Long> {
 
 	@Query("select l from Libelle l where l.langue = :langue")
+	@QueryHints(value = { @QueryHint(name = org.hibernate.jpa.QueryHints.HINT_READONLY, value = "true") })
 	Collection<Libelle> listerParLangue(@Param("langue") String langue);
 
 }
