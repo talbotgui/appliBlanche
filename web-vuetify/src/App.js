b13af464aa253d4af31d537d19ba8910d91e45ff
TALBOT Guillaume <talbotgui@gmail.com>
2018-07-18 21:00:16 +0200

    Vuetify sans moi

--- web-vuetify/src/App.js
+++
@@ -1,8 +0,0 @@
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
--- web-vuetify/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
-<script src="./App.js"></script>
-<style src="./App.css"></style>
