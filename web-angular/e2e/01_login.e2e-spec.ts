49130376c8d7247a6800087def9d9ac279bfbf46
TALBOT Guillaume <talbotgui@gmail.com>
2018-07-24 18:05:12 +0200

    ANGULAR : initier les tests e2e avec bonne gestion des selecteurs

---
+++ web-angular/e2e/01_login.e2e-spec.ts
@@ -0,0 +1,62 @@
+import { ApplicationPage } from './app.po';
+import * as selectors from './selectors';
+import * as path from 'path';
+
+/**
+ * Pour que chaque test soit autonome, chaque test contient un scénario qui redémarre de l'ouverture de la page.
+ * Les étapes de préparation du test sont dans la partie Arrange (de Arrange/Act/Assert) si et seulement si ces étapes ont été testées dans un autre test.
+ *
+ * Pour démarrer le test en DEBUG avec VSCode, il faut lancer "ng serve" depuis un terminal puis depuis la vue DEBUG de lancer le bon test
+ *
+ * Pour n'exécuter qu'un seul test depuis la ligne de commande : "npm run debug-e2e" après avoir modifier le script à lancer dans package.json
+ */
+describe('Page de login', () => {
+  let page: ApplicationPage;
+
+  beforeEach(() => {
+    page = new ApplicationPage();
+  });
+
+  it('Premier accès', () => {
+    //
+    //
+    page.navigateToRoot();
+    //
+    expect(page.isVisible(selectors.PageLogin.TITRE)).toBeTruthy();
+    expect(page.isVisible(selectors.PageLogin.CHAMP_LOGIN)).toBeTruthy();
+    expect(page.isVisible(selectors.PageLogin.CHAMP_MDP)).toBeTruthy();
+    expect(page.isVisible(selectors.PageLogin.BOUTON_CONNECTER)).toBeTruthy();
+    expect(page.isVisible(selectors.APP.DRAPEAU_EN)).toBeTruthy();
+    expect(page.isVisible(selectors.APP.DRAPEAU_FR)).toBeTruthy();
+  });
+
+  it('i18n EN', () => {
+    //
+    //
+    page.navigateToRoot();
+    page.click(selectors.APP.DRAPEAU_EN);
+    //
+    expect(page.getText(selectors.PageLogin.TITRE)).toEqual('Login');
+  });
+
+  it('i18n FR', () => {
+    //
+    //
+    page.navigateToRoot();
+    page.click(selectors.APP.DRAPEAU_FR);
+    //
+    expect(page.getText(selectors.PageLogin.TITRE)).toEqual('Connexion');
+  });
+
+  it('Erreur de connexion', () => {
+    //
+    //
+    page.navigateToRoot();
+    page.type(selectors.PageLogin.CHAMP_LOGIN, 'mauvaisLogin');
+    page.type(selectors.PageLogin.CHAMP_MDP, 'mauvaisMdp');
+    page.click(selectors.PageLogin.BOUTON_CONNECTER);
+    //
+    expect(page.isVisible(selectors.PageLogin.MESSAGE_ERREUR_CONNEXION)).toBeTruthy();
+  });
+
+});
--- web-angular/e2e/selectors.ts
+++ web-angular/e2e/selectors.ts
@@ -1,5 +1,17 @@
 import { by } from 'protractor';
 
 export class APP {
-  static TITRE = by.css('#entete-titre span');
+  static DRAPEAU_FR = by.css("div.drapeaux a.fr");
+  static DRAPEAU_EN = by.css("div.drapeaux a.en");
+}
+
+export class PageLogin {
+  static TITRE = by.css(".container-fluid > h2");
+
+  static CHAMP_LOGIN = by.xpath("//input[@name='login']");
+  static CHAMP_MDP = by.xpath("//input[@name='mdp']");
+
+  static BOUTON_CONNECTER = by.css("button.connecter");
+
+  static MESSAGE_ERREUR_CONNEXION = by.css("div.messageErreur");
 }
