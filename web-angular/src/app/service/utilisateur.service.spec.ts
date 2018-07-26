45fd0154ca3f521756aa17aa5967afa6bac055b8
TALBOT Guillaume <talbotgui@gmail.com>
2018-07-26 08:02:37 +0200

    Suppression des TU qui ne seront pas utilisés dans ce projet

--- web-angular/src/app/service/utilisateur.service.spec.ts
+++
@@ -1,8 +0,0 @@
-import * as model from '../model/model';
-import { UtilisateurService } from '../service/utilisateur.service';
-
-describe('UtilisateurService', () => {
-
-  let controleurAtester: UtilisateurService;
-
-});
