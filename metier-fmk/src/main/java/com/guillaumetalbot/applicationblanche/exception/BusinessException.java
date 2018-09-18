89a62419e7c949a7cafb7da07326ab44b9f21a95
TALBOT Guillaume <talbotgui@gmail.com>
2018-09-18 09:13:45 +0200

    Revue des niveaux de log de certaines exceptions

--- metier-fmk/src/main/java/com/guillaumetalbot/applicationblanche/exception/BusinessException.java
+++ metier-fmk/src/main/java/com/guillaumetalbot/applicationblanche/exception/BusinessException.java
@@ -9,27 +9,27 @@ public class BusinessException extends BaseException {
 	public static final ExceptionId CLIENT_NOM_DEJA_EXISTANT = new ExceptionId("ERREUR_CLIENT_NOM_DEJA_EXISTANT",
 			"Un client nommé '{{0}}' existe déjà", ExceptionLevel.INFORMATION, 400);
 
-	public static final ExceptionId ERREUR_LOGIN = new ExceptionId("ERREUR_LOGIN", "Erreur de connexion", ExceptionLevel.ERROR, 403);
+	public static final ExceptionId ERREUR_LOGIN = new ExceptionId("ERREUR_LOGIN", "Erreur de connexion", ExceptionLevel.WARNING, 403);
 
 	public static final ExceptionId ERREUR_LOGIN_MDP = new ExceptionId("ERREUR_LOGIN_MDP",
-			"Identifiant et/ou mot de passe trop court ({{0}} caractères minimum)", ExceptionLevel.ERROR, 400);
+			"Identifiant et/ou mot de passe trop court ({{0}} caractères minimum)", ExceptionLevel.WARNING, 400);
 
 	public static final ExceptionId ERREUR_LOGIN_VEROUILLE = new ExceptionId("ERREUR_LOGIN_VEROUILLE",
-			"Erreur de connexion - le compte est verrouillé", ExceptionLevel.ERROR, 403);
+			"Erreur de connexion - le compte est verrouillé", ExceptionLevel.WARNING, 403);
 
 	public static final ExceptionId ERREUR_ROLE_NOM = new ExceptionId("ERREUR_ROLE_NOM", "Nom du role trop court ({{0}} caractères minimum)",
-			ExceptionLevel.ERROR, 400);
+			ExceptionLevel.INFORMATION, 400);
 
 	public static final ExceptionId ERREUR_SHA = new ExceptionId("ERREUR_SHA", "Erreur de cryptage", ExceptionLevel.ERROR, 500);
 
 	public static final ExceptionId OBJET_DEJA_EXISTANT = new ExceptionId("ERREUR_OBJET_DEJA_EXISTANT",
-			"Objet de type {{0}} avec l'identifiant {{1}} déjà existant", ExceptionLevel.ERROR, 400);
+			"Objet de type {{0}} avec l'identifiant {{1}} déjà existant", ExceptionLevel.INFORMATION, 400);
 
 	public static final ExceptionId OBJET_NON_EXISTANT = new ExceptionId("ERREUR_OBJET_NON_EXISTANT",
-			"Objet de type {{0}} et de référence {{1}} inexistant", ExceptionLevel.ERROR, 404);
+			"Objet de type {{0}} et de référence {{1}} inexistant", ExceptionLevel.INFORMATION, 404);
 
 	public static final ExceptionId REFERENCE_NON_VALIDE = new ExceptionId("ERREUR_REFERENCE_NON_VALIDE", "Référence {{0}} non valide",
-			ExceptionLevel.ERROR, 404);
+			ExceptionLevel.INFORMATION, 404);
 
 	/** Default UID. */
 	private static final long serialVersionUID = 1L;
