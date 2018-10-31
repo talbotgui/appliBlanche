7ea562b47676f94dd9ffdf79037838db7d8e753b
TALBOT Guillaume <talbotgui@gmail.com>
2018-10-31 16:03:50 +0100

    Correction qualimétrie

--- web-angular/src/app/reservations/page-consommations/page-consommations.component.ts
+++ web-angular/src/app/reservations/page-consommations/page-consommations.component.ts
@@ -2,23 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Language } from 'angular-l10n';
 import { Reservation } from '../model/model';
 
-
 /** Page de gestion des reservations */
 @Component({ selector: 'page-consommations', templateUrl: './page-consommations.component.html', styleUrls: ['./page-consommations.component.css'] })
-export class PageConsommationsComponent implements OnInit {
+export class PageConsommationsComponent {
 
   /** Decorateur nécessaire aux libellés internationnalisés dans des tooltips */
   @Language() lang: string;
 
   /** Liste des réservations en cours */
   reservations: Reservation;
-
-  /** A l'initialisation */
-  ngOnInit() {
-
-
-  }
-
-
-
 }
--- web-angular/src/app/reservations/page-reservations/cadre-reservation/cadre-reservation.component.ts
+++ web-angular/src/app/reservations/page-reservations/cadre-reservation/cadre-reservation.component.ts
@@ -46,7 +46,7 @@ export class CadreReservationComponent implements OnInit {
     this.optionsCalculeesPourLaReservationSelectionnee = {};
     if (this.options && r.options) {
       for (const o of this.options) {
-        const estSelectionnee = (r.options.findIndex((oSel) => o.reference === oSel.reference) >= 0)
+        const estSelectionnee = (r.options.findIndex((oSel) => o.reference === oSel.reference) >= 0);
         this.optionsCalculeesPourLaReservationSelectionnee[o.reference] = estSelectionnee;
       }
     }
