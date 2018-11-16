2534f997aad319aa7344e99752f47873243eab92
TALBOT Guillaume <talbotgui@gmail.com>
2018-11-16 08:04:29 +0100

    Affichage dynamique du tableau

--- web-angular/src/app/administration/page-ressource/page-ressource.component.ts
+++ web-angular/src/app/administration/page-ressource/page-ressource.component.ts
@@ -8,6 +8,7 @@ import { Language } from 'angular-l10n';
 import { DataSourceComponent } from '../../shared/service/datasource.component';
 import { RessourceService } from '../service/ressource.service';
 import * as model from '../../model/model';
+import { RoleService } from '../service/role.service';
 
 /** Page listant les ressources et permettant leur création, modification et suppression */
 @Component({ selector: 'page-ressource', templateUrl: './page-ressource.component.html', styleUrls: ['./page-ressource.component.css'] })
@@ -16,11 +17,17 @@ export class PageRessourceComponent implements OnInit {
   /** Decorateur nécessaire aux libellés internationnalisés dans des tooltips */
   @Language() lang: string;
 
-  /** Liste des colonnes à afficher dans le tableau */
-  displayedColumns: string[] = ['clef', 'description'];
+  /** Liste des colonnes fixes du tableau à générer (générée ne veut pas dire affichée) */
+  colonnesFixes = ['clef', 'description'];
+
+  /** Liste complete des colonnes à afficher */
+  get columnsToDisplay(): string[] { return this.colonnesFixes.concat(this.roles.map((r) => r.nom)); }
 
   /** DataSource du tableau (initialisé dans le onInit) */
   dataSource: DataSourceComponent<model.Ressource>;
+
+  /** Liste des roles existants */
+  roles: model.Role[] = [];
 
   /** Composant de pagination */
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -29,15 +36,22 @@ export class PageRessourceComponent implements OnInit {
   @ViewChild(MatSort) sorter: MatSort;
 
   /** Un constructeur pour se faire injecter les dépendances */
-  constructor(private route: ActivatedRoute, private ressourceService: RessourceService) { }
+  constructor(private route: ActivatedRoute, private ressourceService: RessourceService, private roleService: RoleService) { }
 
   /** Initialisation des composants de la page */
   ngOnInit(): void {
-    // Creation du datasource
-    this.dataSource = new DataSourceComponent<model.Ressource>((page) => this.ressourceService.listerRessources(page));
 
-    // Chargement des données avec les paramètres par défaut (nb éléments par page par défaut défini dans le DataSource)
+    // Chargement des ressources
+    this.dataSource = new DataSourceComponent<model.Ressource>((page) => this.ressourceService.listerRessources(page));
     this.dataSource.load();
+
+    // Chargement des roles
+    const pageDeTousLesRoles = new model.Page<model.Role>(50, 0);
+    this.roleService.listerRoles(pageDeTousLesRoles).subscribe((roles: model.Page<model.Role>) => {
+
+      // Sauvegarde de la liste des roles
+      this.roles = roles.content;
+    });
   }
 
   /** Binding des évènements des composants de la page */
