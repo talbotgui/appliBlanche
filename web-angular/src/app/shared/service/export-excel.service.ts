ee811d62c8e4519063e041ab0fc34230eb252062
TALBOT Guillaume <talbotgui@gmail.com>
2019-02-04 13:15:21 +0100

    Qualimetrie

--- web-angular/src/app/shared/service/export-excel.service.ts
+++ web-angular/src/app/shared/service/export-excel.service.ts
@@ -3,19 +3,35 @@ import * as FileSaver from 'file-saver';
 import * as XLSX from 'xlsx';
 import { HttpProxy } from './httpProxy.component';
 
+/** Type MIME d'un tableau en format OPEN */
 const EXCEL_MIME_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+
+/** Extension MS Excel */
 const EXCEL_EXTENSION = '.xlsx';
 
+/** Composant permettant l'export de document */
 @Injectable()
 export class ExportService {
 
+  /** Constructeur avec injection */
   constructor(private http: HttpProxy) { }
 
+  /**
+   * Méthode exportant le tableau de données dans un fichier Excel avec un seul onglet.
+   * @param json Le tableau contenant des objets. Le nom de l'attribut dans les objets est l'entête des colonnes.
+   * @param nomOnglet Le nom de l'unique onglet
+   * @param nomFichierExcel Le nom du fichier Excel qui sera suffixé de la date et de l'extention
+   */
   public exporterTableauEnExcel(json: any[], nomOnglet: string, nomFichierExcel: string): void {
     const excelBuffer: any = this.creerExcelDepuisTableau(json, nomOnglet);
     this.declencherTelechargement(excelBuffer, nomFichierExcel);
   }
 
+  /**
+   * Méthode créant l'Excel avec les données
+   * @param json Le tableau contenant des objets. Le nom de l'attribut dans les objets est l'entête des colonnes.
+   * @param nomOnglet Le nom de l'unique onglet
+   */
   private creerExcelDepuisTableau(json: any[], nomOnglet: string): any {
     const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
     const workbook: XLSX.WorkBook = XLSX.utils.book_new();
@@ -23,6 +39,11 @@ export class ExportService {
     return XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
   }
 
+  /**
+   * Méthode déclenchant le téléchargement dans le navigateur
+   * @param buffer Buffer de données
+   * @param nomFichier Nom du fichier
+   */
   private declencherTelechargement(buffer: any, nomFichier: string): void {
     const data: Blob = new Blob([buffer], { type: EXCEL_MIME_TYPE });
     const dateDuJour = this.http.formaterDate(new Date());
