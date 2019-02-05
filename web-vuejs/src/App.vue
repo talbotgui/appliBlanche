c96e229f2713662130a21a1ef2616f9634660671
TALBOT Guillaume <talbotgui@gmail.com>
2019-02-05 13:37:59 +0100

    Refonte de l'application VueJS avec la dernière version et en TS

--- web-vuejs/src/App.vue
+++ web-vuejs/src/App.vue
@@ -1,64 +1,29 @@
 <template>
   <div id="app">
-    {{loading}}
-    <router-view v-if="!loading" />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import Vue from 'vue'
-
-export default {
-  name: 'App',
-
-  data () {
-    return {
-      // Libelle en dur car les libellés ne sont pas encore chargés depuis l'API
-      loading: 'Chargement en cours'
-    }
-  },
-
-  mounted () {
-    // Initialisation des paramètres globaux REST (le login est la première méthode rest appelée)
-    Vue.http.options.root = 'http://localhost:9090/applicationBlanche/'
-    Vue.http.options.emulateJSON = true
-
-    // Après un rechargement de la page, si un token JWT est disponible, on le met en place dans les entêtes
-    Vue.http.headers.common['Authorization'] = localStorage.getItem('JWT')
-
-    // Chargement des libelles venant de l'API
-    const locale = 'fr'
-    const cb = (err, message) => {
-      if (err) {
-        console.error(err)
-        return
-      }
-      this.$i18n.setLocaleMessage('fr', message)
-      this.loading = ''
-    }
-    return fetch('http://localhost:9090/applicationBlanche/i18n/' + locale, { method: 'get', headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' } })
-      .then((res) => { return res.json() })
-      .then((json) => {
-        if (Object.keys(json).length === 0) {
-          return Promise.reject(new Error('locale empty !!'))
-        } else {
-          return Promise.resolve(json)
-        }
-      })
-      .then((message) => { cb(null, message) })
-      .catch((error) => { cb(error) })
-  }
-}
-</script>
-
-<style>
+<style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
 }
-.entete {
-  text-align: center;
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
