0c2c866891698c6e51976ecead1ce8ce7c97f36e
TALBOT Guillaume <talbotgui@gmail.com>
2019-02-13 17:47:11 +0100

    Premier tableau

--- web-vuejs/src/pages/page-administration-ressources/page-administration-ressources.ts
+++ web-vuejs/src/pages/page-administration-ressources/page-administration-ressources.ts
@@ -23,13 +23,33 @@ export default class PageAdministrationRessources extends Vue {
     }
 
     /** Méthode appelée dès que le composant est chargé. */
-    public mounted() {
-        this.ressourcesService.listerRessources(this.page).subscribe((p) => {
-            this.page = p;
+    public mounted() { this.chargerDonnees(); }
 
+    /** Au changement du nombre d'élément par page */
+    public selectionnerNbElements(size: number) {
+        this.page.size = size;
+        this.page.number = 0;
+        this.chargerDonnees();
+    }
+
+    /** Au changement d'index de page */
+    public selectionnerIndexPage(pageNumber: number) {
+        this.page.number = pageNumber - 1;
+        this.chargerDonnees();
+    }
+
+    /** Chargement des données */
+    private chargerDonnees() {
+        this.ressourcesService.listerRessources(this.page).subscribe((p) => {
+
+            // Sauvegarde de la page
+            this.page = p;
+            this.page.number = this.page.number + 1;
+
+            // Constutition des indexes de page pour la liste déroulante
             const listeIndexes = [];
             for (let i = 0; i < this.page.totalPages; i++) {
-                listeIndexes.push(i);
+                listeIndexes.push(i + 1);
             }
             this.listeIndexesDePage = listeIndexes;
         });
