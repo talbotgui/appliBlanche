298fdc1a6a51fa6d24685948d9e798774e8e107f
TALBOT Guillaume <talbotgui@gmail.com>
2019-02-19 18:00:43 +0100

    Amélioration du lookdu formulaire de login

--- web-vuejs/src/pages/page-login/page-login.vue
+++ web-vuejs/src/pages/page-login/page-login.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
-    <h2 v-t="'connexion_titre'"></h2>
-    <input type="text" name="login" v-model="login" :placeholder="$t('connexion_identifiant')">
-    <input type="password" autocomplete="current-password" name="password" v-model="mdp" :placeholder="$t('connexion_motDePasse')">
-    <v-btn @click="connecter()" v-t="'connexion_boutonConnexion'"></v-btn>
-  </div>
+	<div>
+		<h2 v-t="'connexion_titre'"></h2>
+		<v-text-field type="text" name="login" v-model="login" :label="$t('connexion_identifiant')"></v-text-field>
+		<v-text-field type="password" autocomplete="current-password" name="password" v-model="mdp" :label="$t('connexion_motDePasse')"></v-text-field>
+		<v-btn @click="connecter()" v-t="'connexion_boutonConnexion'"></v-btn>
+	</div>
 </template>
 
 <script lang="ts" src="./page-login.ts"/>
